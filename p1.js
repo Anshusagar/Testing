f7c571a68387e278ed696a8bf521c585dd53b1cd
Anshusagar <yuvraaj.anshu@gmail.com>
2021-12-07 15:32:22 +0530

    p1 file

---
+++ p1.js
@@ -0,0 +1,22 @@
+//bmi calculator
+function bmiCalculator(weight, height) {
+  return weight / (height * height);
+}
+//read parquet file
+function readParquetFile(filePath) {
+  return filePath;
+}
+//read csv file
+function readCsvFile(filePath) {
+  return filePath;
+}
+//spark session
+function sparkSession() {
+  //spark session
+  return 'spark session';
+}
+
+//function to create spark session
+function createSparkSession() {
+  return sparkSession();
+}
